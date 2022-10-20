3f12cbace56445aa04765698942d7cd04a16789c
devhum7p <devhum7p@gmail.com>
2022-10-20 15:35:17 +0900

    login 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,11 @@
+from multiprocessing import context
 from django.shortcuts import redirect, render
 # from django.contrib.auth.forms import UserCreationForm
 from .forms import CustomUserCreationForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+
 
 # Create your views here.
 def index(request):
@@ -29,3 +33,20 @@ def signup(request):
     }
 
     return render(request, 'accounts/singup.html', context)
+
+
+
+def login(request):
+
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('accounts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form' : form,
+    }
+
+    return render(request, 'accounts/login.html', context)
