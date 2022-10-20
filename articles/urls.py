8a38cf94a0401c454ccb02fca4392818e1387fb4
soohofather <enthchal@gmail.com>
2022-10-20 15:36:36 +0900

    동현 3시 35분 중간저장

--- articles/urls.py
+++ articles/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = "articles"
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path("", views.index, name="index"),
+    path("create/", views.create, name="create"),
+    path("detail/<int:pk>", views.detail, name="detail"),
+    path("delete/<int:pk>", views.delete, name="delete"),
+    path("update/<int:pk>", views.update, name="update"),
 ]
