c52f19f889cec59ee06d6db936d19337d35fb92f
soohofather <enthchal@gmail.com>
2022-10-20 16:33:49 +0900

    동현 4시 30분 중간저장

--- articles/models.py
+++ articles/models.py
@@ -6,3 +6,8 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=80)
     content = models.TextField()
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
--- articles/urls.py
+++ articles/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("detail/<int:pk>", views.detail, name="detail"),
     path("delete/<int:pk>", views.delete, name="delete"),
     path("update/<int:pk>", views.update, name="update"),
+    path("comment/<int:pk>", views.c_create, name="c_create"),
 ]
